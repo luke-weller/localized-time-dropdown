5587f2aa09ceb893c676750cc19da6d8765c4c7f
luke-weller <wellerluke@hotmail.com>
2024-08-06 12:17:57 -0400

    add tests for the analog clock to ensure its being displayed

--- src/components/AnalogClockDisplay/AnalogClockDisplay.test.tsx
+++ src/components/AnalogClockDisplay/AnalogClockDisplay.test.tsx
@@ -1,23 +1,14 @@
+import React from "react";
 import { render, screen } from "@testing-library/react";
 import AnalogClockDisplay from "./AnalogClockDisplay";
-import { ClockDisplayProps } from "../../interfaces";
 
 describe("AnalogClockDisplay", () => {
-  beforeAll(() => {
-    jest.useFakeTimers("modern");
-  });
-
-  afterAll(() => {
-    jest.useRealTimers();
-  });
-
   const renderComponent = (location: string) => {
-    const props: ClockDisplayProps = { location };
-    render(<AnalogClockDisplay {...props} />);
+    render(<AnalogClockDisplay location={location} />);
   };
 
   test("renders without crashing", () => {
     renderComponent("New York");
-    expect(screen.getByTestId("clock")).toBeInTheDocument();
+    expect(screen.getByTestId("clock-container")).toBeInTheDocument();
   });
 });
--- src/components/AnalogClockDisplay/AnalogClockDisplay.tsx
+++ src/components/AnalogClockDisplay/AnalogClockDisplay.tsx
@@ -32,21 +32,23 @@ const AnalogClockDisplay: FC<ClockDisplayProps> = ({ location }) => {
 
   return (
     <div className="clock-container">
-      <Clock
-        className={"clock"}
-        value={time}
-        hourHandWidth={8}
-        hourHandLength={60}
-        minuteHandWidth={4}
-        minuteHandLength={80}
-        secondHandWidth={2}
-        secondHandLength={90}
-        hourMarksWidth={2}
-        hourMarksLength={12}
-        minuteMarksWidth={1}
-        minuteMarksLength={6}
-        size={Math.min(window.innerWidth * 0.8, window.innerHeight * 0.8)}
-      />
+      <div data-testid="clock-container">
+        <Clock
+          className={"clock"}
+          value={time}
+          hourHandWidth={8}
+          hourHandLength={60}
+          minuteHandWidth={4}
+          minuteHandLength={80}
+          secondHandWidth={2}
+          secondHandLength={90}
+          hourMarksWidth={2}
+          hourMarksLength={12}
+          minuteMarksWidth={1}
+          minuteMarksLength={6}
+          size={Math.min(window.innerWidth * 0.8, window.innerHeight * 0.8)}
+        />
+      </div>
     </div>
   );
 };
